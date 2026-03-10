f2520d42f12d2649da73a21713be5a2dcaaaa772
waqasahmadkhan <waqas.ahmad10w@gmail.com>
2026-03-10 06:01:35 -0700

    tokenremoved

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -13,7 +13,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token",accessToken);
+    const token = localStorage.getItem("accessToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
