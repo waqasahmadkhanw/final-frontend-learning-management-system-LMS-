793e1db63f76f696e18a7a8d88d5c0e546215795
waqasahmadkhan <waqas.ahmad10w@gmail.com>
2026-03-10 16:28:43 -0700

    Correctly store access token in local storagere
    Fix saving access token to local storage.

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -165,7 +165,7 @@ const Login = () => {
 
       // Save token for Axios interceptor
       if (accessToken) {
-        localStorage.setItem("token", accessToken);
+        localStorage.setItem("accessToken");
       }
 
       // Save user to context
