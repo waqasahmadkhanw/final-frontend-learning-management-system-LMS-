16aa40cf837a58e4538dbc18cbe76c8dc301c0ee
waqasahmadkhan <waqas.ahmad10w@gmail.com>
2026-03-10 05:42:10 -0700

    refreshToken

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -13,7 +13,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token",accessToken);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
